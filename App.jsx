66bd6f144e148b7a0fb5ab9a3ab27e5d3783b521
Ashborn-web <subhadeepnaskar0032@gmail.com>
2025-04-11 16:05:02 +0530

    Initial upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>AI Stock & Crypto Advisor</h1>
+      <p>Welcome to your AI-powered financial dashboard!</p>
+    </div>
+  );
+}
+
+export default App;
